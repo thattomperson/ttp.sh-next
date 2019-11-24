6f37fe1cdf99520d62fdd4c840cbf6745351e5e6
Thomas Albrighton <thomasalbrighton@gmail.com>
2019-11-25 09:21:24 +1030

    added skills

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -9,13 +9,12 @@
 		position: relative;
 		max-width: 56em;
 		background-color: white;
+    color: #111;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
 </style>
-
-<Nav {segment}/>
 
 <main>
 	<slot></slot>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <style>
-	h1, figure, p {
+	h1, h2, h3, .center {
 		text-align: center;
 		margin: 0 auto;
 	}
@@ -8,39 +8,136 @@
 		font-size: 2.8em;
 		text-transform: uppercase;
 		font-weight: 700;
-		margin: 0 0 0.5em 0;
+		margin: 0.5em 0;
 	}
 
-	figure {
-		margin: 0 0 1em 0;
-	}
+  h2 {
+    font-size: 2em;
+  }
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
+  .section-item h2 {
+    font-weight: 700;
+  }
 
-	p {
-		margin: 1em auto;
-	}
 
 	@media (min-width: 480px) {
 		h1 {
 			font-size: 4em;
 		}
 	}
+
+  .section {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+  }
+
+  .section-item {
+    display: flex;
+    max-width: 28em;
+    min-width: 20em;
+    height: 300px;
+    justify-content: space-around;
+    flex-direction: column;
+    background-size: contain;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+  .section-title {
+    background: rgba(255,255,255,0.5);
+    width: max-content;
+    padding: 10px;
+    margin: 0 auto;
+    
+  }
+
+  .examples {
+    display: flex;
+    flex-wrap: nowrap;
+    justify-content: center;
+  }
+
+  .example {
+    border-radius: 3px;
+    box-shadow: 1px 1px 1px rgba(51,51,51,0.3);
+    padding: 10px;
+    background: white;
+    margin-left: 5px;
+  }
+
+  .avatar {
+    border-radius: 100%;
+  }
+
 </style>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Thomas Albrighton</title>
 </svelte:head>
 
-<h1>Great success!</h1>
+<div class="center">
+  <img alt="Thomas Albrighton's Avatar" class="avatar" src="/photo.png" >
+</div>
+<h1>Thomas Albrighton</h1>
+<h2>DevOps | SysAdmin | Solution Arichtect | Developer</h2>
 
-<figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
-</figure>
+<h1>Skills</h1>
+<div class="section">
+  <div class="section-item" style="background-image: url(/laravel.png)">
+    <div class="section-title">
+      <h2>Laravel</h2>
+      <h3>5 years</h3>
+    </div>
+    <!-- <div class="examples">
+      <a href="https://www.agedcareguide.com.au/" class="example">
+        Aged Care Guide
+      </a>
+      <a href="https://www.agedcareguide.com.au/" class="example">
+        Disability Support Guide
+      </a>
+    </div> -->
+  </div>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+  <div class="section-item" style="background-image: url(/vue.png)">
+    <div class="section-title">
+      <h2>VueJS</h2>
+      <h3>4 Years</h3>
+    </div>
+    <!-- <div class="examples">
+      <a href="https://www.agedcareguide.com.au/" class="example">
+        Aged Care Guide
+      </a>
+      <a href="https://www.agedcareguide.com.au/" class="example">
+        Disability Support Guide
+      </a>
+    </div> -->
+  </div>
+
+  <div class="section-item" style="background-image: url(/svelte.png)">
+    <div class="section-title">
+      <h2>Svelte</h2>
+      <h3>2 Years</h3>
+    </div>
+    <!-- <div class="examples">
+      <a href="https://recommend.ttp.sh/" class="example">
+        Spotify Song Recommendations
+      </a>
+      <a href="https://github.com/thattomperson/ttp.sh" class="example">
+        This site
+      </a>
+    </div> -->
+  </div>
+
+  <div class="section-item" style="background-image: url(/aws.png)">
+    <div class="section-title">
+      <h2>AWS</h2>
+      <h3>6 Years</h3>
+    </div>
+    <!-- <div class="examples">
+      <a href="https://www.agedcareguide.com.au/" class="example">
+        Aged Care Guide
+      </a>
+    </div> -->
+  </div>
+</div>
